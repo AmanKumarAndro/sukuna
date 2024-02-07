02dea513aed365c90febd27974a01f1c852f2bb9
Aman Kumar <amankumar8675@gmail.com>
2024-02-07 23:49:51 +0530

    funnn

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Navbar from './Components/navbar'
 import './App.css'
 
 function App() {
@@ -8,26 +7,69 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Navbar />
+      <div className='container'>
+        <video autoPlay loop muted playsInline className="video">
+          <source src="src/assets/VideoBG.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <div className='content flex justify-center' >
+
+          <section className="py-10 text-white">
+            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+              <div className="mx-auto w-full text-center md:max-w-2xl">
+                <h2 className="text-3xl font-bold leading-tight text-gray-100 sm:text-4xl lg:text-5xl">
+                  Welcome  to our website!
+                </h2>
+                <p className="mx-auto mt-4 max-w-xl text-base leading-relaxed text-gray-300">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
+                  nesciunt eos facilis debitis voluptas iure consectetur odit fugit
+                  voluptate recusandae?
+                </p>
+              </div>
+              <form action="#" method="POST" className="mx-auto mt-12 max-w-xl">
+                <div className="flex flex-col items-center sm:flex-row sm:justify-center">
+                  <div className="flex w-full max-w-sm items-center space-x-2">
+                    <input
+                      className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 "
+                      type="email"
+                      placeholder="Email"
+                    />
+                    <button
+                      type="button"
+                      className="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+                    >
+                      Subscribe
+                    </button>
+                  </div>
+                </div>
+              </form>
+              <div className="mt-8 flex items-center justify-center px-8 sm:px-0">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  className="h-4 w-4 text-gray-600"
+                >
+                  <rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect>
+                  <path d="M7 11V7a5 5 0 0 1 10 0v4"></path>
+                </svg>
+                <span className="ml-2 text-sm text-gray-400">
+                  Your data is complely secured with us. We don&#x27;t share with anyone.
+                </span>
+              </div>
+            </div>
+          </section>
+        </div>
+
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+
+    <nav className="bg-gray-700">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a href='-' className="flex items-center space-x-3 rtl:space-x-reverse">
+            
+            <span className="self-center text-2xl font-bold whitespace-nowrap text-red-400 ">Sukuna <span className='text-red-700 '>Loves</span>  Megumi</span>
+        </a>
+        <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm rounded-lg md:hidden" aria-controls="navbar-default" aria-expanded="false">
+            <span className="sr-only">Open main menu</span>
+            <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+            </svg>
+        </button>
+        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border rounded-lg  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0  ">
+            <li>
+              <a href="#" className="block py-2 px-3 text-blue-500  outline-double" aria-current="page">Home</a>
+            </li>
+            <li>
+              <a href="#" className="block py-2 px-3">About</a>
+            </li>
+           
+            <li>
+              <a href="#" className="block py-2 px-3">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
